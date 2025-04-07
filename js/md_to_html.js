c2d97221cf6be443c7d506dab5fdbae017c4340a
ben-aaron188 <ben-aaron188@users.noreply.github.com>
2025-04-07 09:43:34 +0200

    updated

--- js/md_to_html.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-
-  var body_from_html = $("body").html();
-
-  var converter = new showdown.Converter(),
-    text = body_from_html,
-    html_ = converter.makeHtml(text);
-  $('body').html(html_);
-});
